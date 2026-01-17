246fba6e64216441a02cc0465fd8f2f592165dc4
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-01-17 19:05:22 +0200

    Style header + footer text, add blur too

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <h3>
+      <h3 className="footer-text">
         Developed with ❤️ by{" "}
         <a href="https://github.com/ArtemiiHH" target="_blanc">
           Barashkov Artemii
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <h1>Geography Memo Game</h1>
+      <h1 className="game-title">Geography Memo Game</h1>
     </header>
   );
 }
