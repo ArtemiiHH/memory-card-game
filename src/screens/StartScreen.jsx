fc31cdc54678967496c5f9eea9608a393a85c697
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-01-17 18:21:12 +0200

    Style start screen (basic)

--- src/screens/StartScreen.jsx
+++ src/screens/StartScreen.jsx
@@ -1,6 +1,8 @@
+import backgroundImage from "../assets/background-image4.png";
+
 export default function StartScreen() {
   return (
-    <div className="start-screen-button-group">
+    <div className="start-screen-modal">
       <button>Easy</button>
       <button>Medium</button>
       <button>Hard</button>
