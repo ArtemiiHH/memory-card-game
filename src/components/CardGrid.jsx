523fce3615bb3d44f267aff48bc6d42784e31416
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-01-18 16:26:09 +0200

    Map through listCountries and get name, flag

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -5,9 +5,20 @@ import Card from "./Card";
 export default function CardGrid() {
   const [listCountries, setListCountries] = useState([]);
 
+  // Run effect once, when component appears
+  useEffect(() => {
+    const countries = getCountries();
+    setListCountries(countries);
+  }, []);
+
   return (
     <div className="card-grid">
-      
+      {listCountries.map((country) => {
+        <Card
+          name={country.name}
+          flag={country.flag}
+        ></Card>
+      })}
     </div>
   );
 }
