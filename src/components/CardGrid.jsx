f76206571752f30777314e151b5c10e4982be44e
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-01-19 16:43:50 +0200

    Remove fetch logic from CardGrid

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,28 +1,3 @@
-import { useEffect, useState } from "react";
-import { getCountries } from "../data/fetchApi";
-import Card from "./Card";
-
 export default function CardGrid() {
-  // Store final list of countries
-  const [listCountries, setListCountries] = useState([]);
-
-  // Run effect once, when component appears
-  useEffect(() => {
-    async function fetchCountries() {
-      // Wait for API to finish
-      const countries = await getCountries();
-      // Store resolved data
-      setListCountries(countries);
-    }
-
-    fetchCountries();
-  }, []); // Run once on mount
-
-  return (
-    <div className="card-grid">
-      {listCountries.map((country) => (
-        <Card key={country.name} name={country.name} flag={country.flag}></Card>
-      ))}
-    </div>
-  );
+  return <div className="card-grid"></div>;
 }
