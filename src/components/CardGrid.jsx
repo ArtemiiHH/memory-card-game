dc9f527167c1f5726495670b84d8a7b761e7915a
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-01-19 22:21:52 +0200

    Write function to get 5 random countries from data

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,11 +1,18 @@
 import Card from "../components/Card";
 
 export default function CardGrid({ countriesList }) {
+  // Shuffle cards array and get random countries
+  function shuffleArray(array) {
+    return [...array].sort(() => Math.random() - 0.5);
+  }
+
   return (
     <div className="card-grid">
-      {countriesList.slice(0, 5).map((country) => (
-        <Card name={country.name} flag={country.flag}></Card>
-      ))}
+      {shuffleArray(countriesList)
+        .slice(0, 5)
+        .map((country) => (
+          <Card name={country.name} flag={country.flag}></Card>
+        ))}
     </div>
   );
 }
