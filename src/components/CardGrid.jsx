33d3b90fae75bf36a9f10fc5c98ca2fedd3a5330
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-01-20 22:13:56 +0200

    Remove shuffle logic, move to App

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,18 +1,5 @@
 import Card from "../components/Card";
 
-export default function CardGrid({ countriesList }) {
-  // Shuffle cards array and get random countries
-  function shuffleArray(array) {
-    return [...array].sort(() => Math.random() - 0.5);
-  }
-
-  return (
-    <div className="card-grid">
-      {shuffleArray(countriesList)
-        .slice(0, 5)
-        .map((country) => (
-          <Card name={country.name} flag={country.flag}></Card>
-        ))}
-    </div>
-  );
+export default function CardGrid() {
+  return <div className="card-grid"></div>;
 }
