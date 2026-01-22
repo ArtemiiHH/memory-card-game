f0c57f5c8fe80c260fe90102863d5ce5d055581c
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-01-22 19:49:38 +0200

    Remove wrong arrow function from onClick

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ name, flag, getCardClicks }) {
   return (
-    <div className="card" onClick={() => getCardClicks()}>
+    <div className="card" onClick={getCardClicks}>
       <img className="country-flag" src={flag} />
       <h3 className="country-name">{name}</h3>
     </div>
