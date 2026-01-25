bc4a3a43834ceb6fd17a5e64212c17b7279e016b
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-01-25 16:53:15 +0200

    Create sound button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 export default function Header() {
   return (
     <header>
+      <button className="sound-btn">🔇</button>
       <a href="">
         <h1 className="game-title">
           Ge<span className="globe-emoji">🌎</span>graphy Memo Game
